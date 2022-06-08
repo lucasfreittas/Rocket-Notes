34779cb67986a2d23563282708c27473d45dae88
lucasfreittas <l.freittas@outlook.com>
2022-06-08 20:05:47 -0300

    Finishing first page

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./styles"; // Pegando estilo CSS
+import { Container, Links, Content } from "./styles"; // Pegando estilo CSS
 import { Header } from "../../components/Header"; 
 import { Button } from "../../components/Button"; // Pegando o botão
 import { Section } from "../../components/Section";
@@ -11,28 +11,46 @@ export function Details(){ //Renderizando na pagina
   return(
     <Container> 
       <Header/>
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota"/>
 
-        <ButtonText title="Excluir Nota"/>
-        <Section title="Links úteis">
-          <Links>
-            <li>
-              <a href="#">https://www.rocketseat.com.br</a>
-            </li>
-            <li>
-              <a href="#">https://www.rocketseat.com.br</a>
-            </li>
-         
-          </Links>
-        </Section>
+            <h1>
+              Introdução ao React
+            </h1>
+            <p>
+              lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet,
+              consectetur lorem ipsum dolor sit amet, consectetur
+              lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet,
+              consectetur lorem ipsum dolor sit amet, consectetur
+              lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet,
+              consectetur lorem ipsum dolor sit amet, consectetur
+              lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet,
+              consectetur lorem ipsum dolor sit amet, consectetur
+              lorem ipsum dolor sit amet, consectetur lorem ipsum dolor sit amet,
+              consectetur lorem ipsum dolor sit amet, consectetur
+            </p>
 
-        <Section title = "Marcadores">
-          <Tag title="Express" />
-          <Tag title="NodeJS" />
-        </Section>
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>   
+            </Links>
+          </Section>
 
-        <Button title="Voltar"/>
+          <Section title = "Marcadores">
+            <Tag title="Express" />
+            <Tag title="NodeJS" />
+          </Section>
 
+          <Button title="Voltar"/>
 
+        </Content>
+      </main>
     </Container>
   )
 }
