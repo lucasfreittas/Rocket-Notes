e4c8808a9d521244fdb38b71215e920de686736f
lucasfreittas <l.freittas@outlook.com>
2022-06-08 19:48:27 -0300

    Links and Tags Components

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,13 +1,33 @@
-import { Container } from "./styles"; // Pegando estilo CSS
+import { Container, Links } from "./styles"; // Pegando estilo CSS
 import { Header } from "../../components/Header"; 
 import { Button } from "../../components/Button"; // Pegando o botão
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 
 export function Details(){ //Renderizando na pagina
   
   return(
     <Container> 
-      <Header></Header>
+      <Header/>
+
+        <Section title="Links úteis">
+          <Links>
+            <li>
+              <a href="#">https://www.rocketseat.com.br</a>
+            </li>
+            <li>
+              <a href="#">https://www.rocketseat.com.br</a>
+            </li>
+         
+          </Links>
+        </Section>
+
+        <Section title = "Marcadores">
+          <Tag title="Express" />
+          <Tag title="NodeJS" />
+        </Section>
+
         <Button title="Voltar"/>
 
 
