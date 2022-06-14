bbe60df7e589774211695923e91640592b42ff39
lucasfreittas <l.freittas@outlook.com>
2022-06-14 18:44:09 -0300

    Profile Page

---
+++ rocketnotes/src/pages/Profile/index.jsx
@@ -0,0 +1,56 @@
+import {FiArrowLeft, FiUser, FiMail, FiLock, FiCamera} from 'react-icons/fi';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+import { Container, Form, Avatar } from './styles';
+
+
+export function Profile(){
+    return(
+        <Container>
+            <header>
+                <a href="/">
+                    <FiArrowLeft/>
+                </a>
+            </header>
+
+            <Form>
+                <Avatar>
+                    <img src="https://github.com/lucasfreittas.png" alt="Foto do Usuário" />
+                    <label htmlFor="avatar">
+                        <FiCamera/>
+
+                        <input
+                            id="avatar"
+                            type="file"
+                        />
+                    </label>
+                </Avatar>
+                <Input
+                    placeholder="Nome"
+                    type="Text"
+                    icon={FiUser}
+                />
+
+                <Input
+                    placeholder="Email"
+                    type="Text"
+                    icon={FiMail}
+                />
+
+                <Input
+                    placeholder="Senha Atual"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Input
+                    placeholder="Nova Senha"
+                    type="Text"
+                    icon={FiLock}
+                />
+
+                <Button title="Salvar"/>
+            </Form>
+        </Container>
+    )
+}
